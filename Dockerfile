41cde6afe5f6037e117174da8171629fadf66078
koarakko5555 <hosayaan.1016@gmail.com>
2025-01-05 22:03:22 +0900

    todoリストのCRUD操作を実装

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,29 @@
-# Dockerfile
 FROM ruby:3.0.4
 
-# 以下はそのまま
+# 必要なパッケージをインストール
 RUN apt-get update -qq && apt-get install -y nodejs npm && npm install -g yarn
+
+# 再発防止: UIDとGIDを指定してグループとユーザーを作成
+ARG UID=1000
+ARG GID=1000
+RUN groupadd -g $GID hosayan1016 && \
+    useradd -m -u $UID -g $GID hosayan1016
+
+# 作業ディレクトリを設定
 WORKDIR /app
+
+# ファイルをコピーし、依存関係をインストール
 COPY Gemfile Gemfile.lock ./
 RUN bundle install
+
+# アプリケーションのコードをコンテナにコピー
 COPY . .
+
+# ポートを公開
 EXPOSE 3000
+
+# 非rootユーザーに切り替え
+USER hosayan1016
+
+# Railsサーバーを起動
 CMD ["rails", "server", "-b", "0.0.0.0"]
--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,50 @@
 class TodosController < ApplicationController
+  before_action :set_todo, only: %i[show edit update destroy]
+
+  def index
+    @todos = Todo.all
+  end
+
+  def show
+    @todo = Todo.find(params[:id])
+  end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+    if @todo.save
+      redirect_to todos_path, notice: 'ToDoが作成されました。'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @todo.update(todo_params)
+      redirect_to todos_path, notice: 'ToDoが更新されました。'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @todo.destroy
+    redirect_to todos_path, notice: 'ToDoが削除されました。'
+  end
+
+  private
+
+  def set_todo
+    @todo = Todo.find(params[:id])
+  end
+
+  def todo_params
+    params.require(:todo).permit(:title, :description, :deadline, :status)
+  end
 end
