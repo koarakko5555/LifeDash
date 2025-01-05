4152cf827fedd5cac42986517d97c2ed714c6245
koarakko5555 <hosayaan.1016@gmail.com>
2025-01-06 07:44:57 +0900

    rootをtodoのhomeに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'posts#index' # rootをposts#indexに変更
+  root 'todos#index' # rootをposts#indexに変更
 
   resources :posts, only: %i[new create show index destroy edit update]
   resources :todos
